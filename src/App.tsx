12b14fb39b5da519bbf916aaaa6bef23dbac9ae8
Jobulikannan <kannan@jobuli.com>
2026-03-11 21:19:00 +0530

    basic props completed
    basic props done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import "./App.css";
 import { Greet } from "./components/Greet";
+import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
 
 function App() {
+  const personName = {
+    first: "Kannan",
+    last: "Solaimalai",
+  };
+
+  const nameList = [
+    {
+      first: "Bruce",
+      last: "Wayne",
+    },
+    {
+      first: "Clark",
+      last: "Kent",
+    },
+    {
+      first: "Diana",
+      last: "Prince",
+    },
+  ];
   return (
     <div className="App">
-      <Greet name="Kannan" />
+      <Greet name="Kannan" messageCount={20} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
