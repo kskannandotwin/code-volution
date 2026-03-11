d8833ded5b8d4889901435f3b316158016f1c58c
Jobulikannan <kannan@jobuli.com>
2026-03-11 21:57:27 +0530

    advanced props completed
    advanced props done

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,18 @@
 import "./App.css";
 import { Greet } from "./components/Greet";
-import { Person } from "./components/Person";
-import { PersonList } from "./components/PersonList";
+import { Heading } from "./components/Heading";
+import { Oscar } from "./components/Oscar";
+import { Status } from "./components/Status";
 
 function App() {
-  const personName = {
-    first: "Kannan",
-    last: "Solaimalai",
-  };
-
-  const nameList = [
-    {
-      first: "Bruce",
-      last: "Wayne",
-    },
-    {
-      first: "Clark",
-      last: "Kent",
-    },
-    {
-      first: "Diana",
-      last: "Prince",
-    },
-  ];
   return (
     <div className="App">
-      <Greet name="Kannan" messageCount={20} isLoggedIn={false} />
-      <Person name={personName} />
-      <PersonList names={nameList} />
+      <Status status="loading" />
+      <Heading>Placeholder Text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to Leonardo DiCaprio</Heading>
+      </Oscar>
+      <Greet name="Codevolution" isLoggedIn={false} />
     </div>
   );
 }
