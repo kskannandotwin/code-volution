22ed8781d55f2c5a7712048616eaf417d56586fd
Jobulikannan <kannan@jobuli.com>
2026-03-17 19:13:31 +0530

    generic props completed
    generic props done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import "./App.css";
-import { Private } from "./components/auth/Private";
-import { Profile } from "./components/auth/Profile";
+import { List } from "./components/generics/List";
 
 function App() {
   return (
     <div className="App">
-      <Private isLoggedIn={true} component={Profile} />
+      {/* <List
+        items={["Bat man", "Superman", "Wonder Woman"]}
+        onClick={(item) => {
+          console.log(item);
+        }}
+      />
+      <List items={[1, 2, 3]} onClick={(item) => console.log(item)} /> */}
+      <List
+        items={[
+          { id: 1, first: "Bruce", last: "Wayne" },
+          { id: 2, first: "Clark", last: "Kent" },
+          { id: 3, first: "Diana", last: "Prince" },
+        ]}
+        onClick={(item) => console.log(item)}
+      />
     </div>
   );
 }
