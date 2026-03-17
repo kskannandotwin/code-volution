657ff9adbc32fbdaf82d5768c1e79235e0a28290
Jobulikannan <kannan@jobuli.com>
2026-03-17 19:38:27 +0530

    restricting props completed
    restricting props done

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import "./App.css";
-import { List } from "./components/generics/List";
+import { RandomNumber } from "./components/restriction/RandomNumber";
 
 function App() {
   return (
     <div className="App">
-      {/* <List
-        items={["Bat man", "Superman", "Wonder Woman"]}
-        onClick={(item) => {
-          console.log(item);
-        }}
-      />
-      <List items={[1, 2, 3]} onClick={(item) => console.log(item)} /> */}
-      <List
-        items={[
-          { id: 1, first: "Bruce", last: "Wayne" },
-          { id: 2, first: "Clark", last: "Kent" },
-          { id: 3, first: "Diana", last: "Prince" },
-        ]}
-        onClick={(item) => console.log(item)}
-      />
+      <RandomNumber value={10} isPositive />
     </div>
   );
 }
